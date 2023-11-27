3b25d8803f48bf4a8b8764ef354f5a507b1837a9
Giuseppe Caferra <g.caferra@icloud.com>
2023-11-27 17:26:50 +0100

    typo fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { inter} from "@/app/ui/fonts";
 import  { Metadata } from "next";
 
 export const metadata : Metadata = {
-  title: 'Acme Dashnoard',
+  title: 'Acme Dashboard',
   description: 'The official Next.js course dashboard',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 }
